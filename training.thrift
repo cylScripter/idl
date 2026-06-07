55753fcf2e1e8a01f2a0e09c255294a19ef3ac03
cyl <2842871262@qq.com>
2026-06-08 02:23:56 +0800

    fix

--- training.thrift
+++ training.thrift
@@ -298,7 +298,10 @@ struct EditPracticeTeachingStatsReq {
     13: i32 external_instructor_count(go.tag='json:"external_instructor_count"'); // 校外指导教师总数
     14: string safety_measures(go.tag='json:"safety_measures"'); // 校内实践安全管理与举措
     19: i32 has_emergency_plan(go.tag='json:"has_emergency_plan"'); // 是否制定紧急预案 1是 2否
-    20: i32 is_delete(go.tag='json:"has_safety_measures"'); // 1 删除
+    18: i32 type(go.tag='json:"course_type" gorm:"column:course_type;default:1"');  // 1  校内  2  校外
+    20: i32 outside_practice_total_weeks(go.tag='json:"outside_practice_total_weeks" gorm:"column:outside_practice_total_weeks"'); // 校外实践总周数
+    21: i32 location_is_shenzhen(go.tag='json:"location_is_shenzhen" gorm:"column:location_is_shenzhen;default:2"'); //// 校外实践场所是否均在深圳
+    22: i32 is_delete(go.tag='json:"has_safety_measures"'); // 1 删除
 }
 
 struct EditPracticeTeachingStatsResp {
